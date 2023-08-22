a968a0de68e6d980c2f0908afbfc5ca13e798ef6
shubhamkumar-oracle <141868106+shubhamkumar-oracle@users.noreply.github.com>
2023-08-22 00:02:25 +0000

    Add build and test jobs, add manual trigger of workflows and add testing flag in Makefile

--- tests/VM/vm.sh
+++ tests/VM/vm.sh
@@ -4,10 +4,7 @@
 export LD_LIBRARY_PATH=`git rev-parse --show-toplevel`
 cd $LD_LIBRARY_PATH
 cd tests/VM
-rm vm_test
 cc -I $LD_LIBRARY_PATH -std=gnu99 -o vm_test vm_test.c -L $LD_LIBRARY_PATH -lresource
-rm ./vm_info.orig
-rm ./vm_info.txt
 cat /proc/vmstat > ./vm_info.orig
 ./vm_test
 diff ./vm_info.orig ./vm_info.txt
