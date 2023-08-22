be21062d505a65798a0d61481f7f8ba96e308c4a
shubhamkumar-oracle <141868106+shubhamkumar-oracle@users.noreply.github.com>
2023-08-22 00:02:25 +0000

    Add build and test jobs, add manual trigger of workflows and add testing flag in Makefile

--- tests/CPU/cpu.sh
+++ tests/CPU/cpu.sh
@@ -3,8 +3,6 @@ export LD_LIBRARY_PATH=`git rev-parse --show-toplevel`
 cd $LD_LIBRARY_PATH
 cd tests/CPU
 cc -I $LD_LIBRARY_PATH -std=gnu99 -o cpu_test cpu_test.c -L $LD_LIBRARY_PATH -lresource
-rm ./cpu_info.orig
-rm ./cpu_info.txt
 cat /proc/cpuinfo > ./cpu_info.orig
 ./cpu_test
 diff ./cpu_info.orig ./cpu_info.txt
